652fe3fd6ef923c7f69684dbf8b2a2c6f1fff2d9
Мирослав Антоневич <miroslav.antonevich@gmail.com>
2026-08-19 01:06:03 +0300

    complete tasks 3 and 4

---
+++ homework-8.js
@@ -0,0 +1,29 @@
+// Задание 3
+
+const userMiroslav = {
+    firstName: "Мирослав",
+    lastName: "Антоневич",
+    mail: "Miroslav.Antonevich@mail.com",
+    age: 27,
+    job: "Менеджер по продажам",
+    country: "Россия",
+    city: "Москва",
+    gender: "Мужчина",
+    relationshipStatus: "Женат",
+    religion: "Ислам",
+};
+
+// Задание 4
+
+const userCar = {
+    makeCar: "Lexus",
+    modelCar: "IS F",
+    yearOfRelease: "2012",
+    carColor: "Черный",
+    transmission: "8-ступенчатый автомат (SPDS)",
+};
+
+userCar.carOwner = userMiroslav;
+console.log(userCar);
+
+// Задание 5
